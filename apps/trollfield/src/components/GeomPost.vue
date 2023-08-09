03efa88be0ab3167ae56b6f8c1aa9a95eef353b1
lislis <mail@lislis.de>
2023-08-09 15:07:09 +0200

    current state

---
+++ apps/trollfield/src/components/GeomPost.vue
@@ -0,0 +1,31 @@
+<template>
+    <Box :position="position"
+         @pointerOver="onPointerOver"
+         @click="onPointerClick">
+        <PhongMaterial :color="materialColor" />
+    </Box>
+</template>
+<script>
+export default {
+     name: "GeomPost",
+     props: [ 'key', 'post' ],
+     data() {
+         return {
+             materialColor: '#dd0000',
+         }
+     },
+     methods: {
+         onPointerOver({over}) {
+             this.materialColor = over ? '#00ff00' : '#ff0000';
+         },
+         onPointerClick(e) {
+             console.log(e);
+         }
+     },
+     computed: {
+         position() {
+             return {x: this.key + 1 , y: this.key + 1, z: -5 };
+         }
+     }
+}
+</script>
--- apps/trollfield/src/views/Home.vue
+++ apps/trollfield/src/views/Home.vue
@@ -10,8 +10,8 @@
             Loading
         </div>
 
-        <Renderer ref="renderer" resize="window" v-if="store.allPosts.length != 0">
-            <Camera :position="{ y: 5, z: 15 }" />
+        <Renderer ref="renderer" resize orbit-ctrl pointer v-if="store.allPosts.length != 0">
+            <Camera :position="{ y: 8, z: 18 }" />
             <Scene ref="scene" background="#cccccc">
                 <DirectionalLight color="#ffffff" intensity="3"
                                   :position="{x: 1, y: 1, z: 1}" />
@@ -19,23 +19,33 @@
                                   :position="{x: -1, y: -1, z: -1}" />
                 <AmbientLight color="#555555" />
 
-
                 <Box v-for="(val, key) in store.allPosts" :key="key" :ref="`mesh-${key}`"
-                     :position="position(key)">
-                    <LambertMaterial />
+                     :position="position(key)"
+                     @pointerEnter="onPointerOver"
+                     @pointerLeave="onPointerOver"
+                     @click="onPointerClick(val)">
+                    <PhongMaterial color="#667788" />
                 </Box>
 
             </Scene>
+
         </Renderer>
     </div>
 </template>
 
 <script>
  import { usePostsStore } from '@/stores/posts'
-
+ import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+3
  export default {
      name: "TrollfieldHomeView",
      components: { },
+     data() {
+         return {
+             baseColor: { r: 0.58, g: 0.14, b: 0.47},
+             hoverColor: { r: 0.3, g: 0.3, b: 0.9},
+         }
+     },
      created() {
          isLoading: false
          //window.scroll({top: 0, behavior: 'smooth'});
@@ -45,17 +55,34 @@
          return { store }
      },
      mounted() {
-         const renderer = this.$refs.renderer;
-        // const box = this.$refs.box.mesh;
-        // renderer.onBeforeRender(() => {
-        //     box.rotation.x += 0.01;
-         // });
+         //const renderer = this.$refs.renderer;
+         //const orbitCtrl = renderer.three.cameraCtrl;
 
-         //debugger
+         //orbitCtrl.enablePan = false;
+         //const box = this.$refs.box.mesh;
+         //renderer.onBeforeRender(() => {
+            // box.rotation.x += 0.01;
+         //});
      },
      methods: {
          position(i) {
-             return {x: i + 1 , y: i + 1, z: -5 };
+             return {x: Math.random() * 10 , y: 0, z: Math.random() * 10 };
+         },
+         onPointerOver({over, component}) {
+             if (over) {
+                 //console.log(component.material.color)
+                 component.material.color = this.hoverColor;
+             } else {
+                 component.material.color = this.baseColor;
+             }
+         },
+         onPointerClick(e) {
+             //console.log(e);
+             this.$router.push(e.slug)
+             //debugger
+         },
+         materialColor() {
+             return
          }
      }
  }
