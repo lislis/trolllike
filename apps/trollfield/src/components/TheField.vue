1dce6f0c70403d53a02037f71be89cf402707ad3
lislis <mail@lislis.de>
2023-10-31 20:39:20 +0100

    bring back click

--- apps/trollfield/src/components/TheField.vue
+++ apps/trollfield/src/components/TheField.vue
@@ -16,6 +16,15 @@
                       id="camera"
                       look-controls
                       position="0 1.6 0">
+                <a-entity cursor="fuse: true"
+                          animation__click="property: scale; startEvents: click; easing: easeInCubic; dur: 150; from: 0.1 0.1 0.1; to: 1 1 1"
+                          animation__fusing="property: scale; startEvents: fusing; easing: easeInCubic; dur: 1500; from: 1 1 1; to: 0.1 0.1 0.1"
+                          animation__mouseleave="property: scale; startEvents: mouseleave; easing: easeInCubic; dur: 500; to: 1 1 1"
+
+                          position="0 0 -1"
+                          geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+                          material="color: black; shader: flat">
+                </a-entity>
             </a-entity>
         </a-entity>
 
