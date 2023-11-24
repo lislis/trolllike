83cf98036e8542a103e4b022aa85d26ba36729ad
lislis <mail@lislis.de>
2023-11-24 14:35:55 +0100

    add more ui buttons

--- apps/trollfield/src/components/TheField.vue
+++ apps/trollfield/src/components/TheField.vue
@@ -7,7 +7,7 @@
             </AAsset>
         </a-assets>
 
-        <a-entity environment="dressing: none; groundYScale: 11.16; ground: hills; skyColor: #aaa; groundColor2: #adadad; grid: 1x1; dressingScale: 2.1; shadowSize: 10.13; groundTexture: squares; active: true; skyType: gradient; horizonColor: #404040; lightPosition: [object Object]; fog: 0.81; flatShading: true; groundColor: #252525; dressingColor: #9f9f9f; dressingVariance: [object Object]; preset: checkerboard"></a-entity>
+        <a-entity :environment="environment"></a-entity>
 
         <!-- PLAYER -->
         <a-entity id="rig"
@@ -30,9 +30,10 @@
             </a-entity>
         </a-entity>
 
-        <a-light type="ambient" color="#ccc"></a-light>
-        <a-light color="#ddf" distance="100" intensity="0.4" type="point"></a-light>
-        <a-light color="#ddf" position="3 10 -10" distance="50" intensity="0.4" type="point"></a-light>
+        <a-light type="ambient" color="#ccc" visible="false"></a-light>
+        <a-light color="#ddf" distance="100" visible="true" intensity="0.4" type="point"></a-light>
+        <a-light color="#ddf" position="3 10 -10" distance="50" visible="false" intensity="0.4" type="point"></a-light>
+
 
         <a-entity position="0 0 0" id="all-posts">
         <APost  v-for="(post, i) in store.getPostsByCurrentFilter"
@@ -47,12 +48,9 @@
     </div>
 
     <div class="ui">
-        <button class="magic-window"
-                @click="toggleMagicWindow"
-                :class="{ 'is-active': isMagicWindow}">
-            <span class="a11y-hidden">Toggle mobile window tracking</span>
-            <svg fill="black" viewBox="0 0 512 512"><path d="M464 6.1c9.5-8.5 24-8.1 33 .9l8 8c9 9 9.4 23.5 .9 33l-85.8 95.9c-2.6 2.9-4.1 6.7-4.1 10.7V176c0 8.8-7.2 16-16 16H384.2c-4.6 0-8.9 1.9-11.9 5.3L100.7 500.9C94.3 508 85.3 512 75.8 512c-8.8 0-17.3-3.5-23.5-9.8L9.7 459.7C3.5 453.4 0 445 0 436.2c0-9.5 4-18.5 11.1-24.8l111.6-99.8c3.4-3 5.3-7.4 5.3-11.9V272c0-8.8 7.2-16 16-16h34.6c3.9 0 7.7-1.5 10.7-4.1L464 6.1zM432 288c3.6 0 6.7 2.4 7.7 5.8l14.8 51.7 51.7 14.8c3.4 1 5.8 4.1 5.8 7.7s-2.4 6.7-5.8 7.7l-51.7 14.8-14.8 51.7c-1 3.4-4.1 5.8-7.7 5.8s-6.7-2.4-7.7-5.8l-14.8-51.7-51.7-14.8c-3.4-1-5.8-4.1-5.8-7.7s2.4-6.7 5.8-7.7l51.7-14.8 14.8-51.7c1-3.4 4.1-5.8 7.7-5.8zM87.7 69.8l14.8 51.7 51.7 14.8c3.4 1 5.8 4.1 5.8 7.7s-2.4 6.7-5.8 7.7l-51.7 14.8L87.7 218.2c-1 3.4-4.1 5.8-7.7 5.8s-6.7-2.4-7.7-5.8L57.5 166.5 5.8 151.7c-3.4-1-5.8-4.1-5.8-7.7s2.4-6.7 5.8-7.7l51.7-14.8L72.3 69.8c1-3.4 4.1-5.8 7.7-5.8s6.7 2.4 7.7 5.8zM208 0c3.7 0 6.9 2.5 7.8 6.1l6.8 27.3 27.3 6.8c3.6 .9 6.1 4.1 6.1 7.8s-2.5 6.9-6.1 7.8l-27.3 6.8-6.8 27.3c-.9 3.6-4.1 6.1-7.8 6.1s-6.9-2.5-7.8-6.1l-6.8-27.3-27.3-6.8c-3.6-.9-6.1-4.1-6.1-7.8s2.5-6.9 6.1-7.8l27.3-6.8 6.8-27.3c.9-3.6 4.1-6.1 7.8-6.1z"/></svg>
-        </button>
+        <MagicWindowButton @magicWindowChanged="toggleMW" />
+        <DaytimeButton v-if="!isRagnarok" @daytimeChanged="toggleDT" />
+        <RagnarokButton @rChanged="toggleR" />
     </div>
 </template>
 
@@ -61,17 +59,23 @@
  import { usePostsStore } from '@/stores/posts'
  import APost from '@/components/APost.vue';
  import AAsset from '@/components/AAsset.vue';
+ import MagicWindowButton from '@/components/MagicWindowButton.vue';
+ import DaytimeButton from '@/components/DaytimeButton.vue';
+ import RagnarokButton from '@/components/RagnarokButton.vue';
 
  export default {
      name: "TheField",
-     components: {APost, AAsset},
+     components: {APost, AAsset,
+                  MagicWindowButton, DaytimeButton, RagnarokButton},
      created() {
-       isLoading: false;
-       //console.log(this.points);
+         isLoading: false;
+         //console.log(this.points);
      },
      data() {
          return {
-             isMagicWindow: false
+             isMagicWindow: false,
+             isDay: true,
+             isRagnarok: false
          }
      },
      setup() {
@@ -79,16 +83,43 @@
          return { store }
      },
      methods: {
-         toggleMagicWindow() {
-             this.isMagicWindow = !this.isMagicWindow
+         toggleMW(state) {
+             this.isMagicWindow = state;
          },
+         toggleDT(state) {
+             //console.log(state);
+             this.isDay = state;
+         },
+         toggleR(state) {
+             this.isRagnarok = state;
+         }
      },
      computed: {
          magicWindow() {
-             //console.log(`magicWindowTrackingEnabled: ${this.isMagicWindow}`);
              return `magicWindowTrackingEnabled: ${this.isMagicWindow}`;
-         }
+         },
+         environment() {
+             let skycolor, horizoncolor, fog;
 
+             if (!this.isRagnarok) {
+                 if (this.isDay) {
+                     skycolor = '#1c71d8';
+                     horizoncolor = '#99c1f1';
+                     fog = 0.4;
+                 } else {
+                     skycolor = '#192636';
+                     horizoncolor = '#05111f';
+                     fog = 0.9;
+                 }
+             } else {
+                 skycolor = '#a51d2d';
+                 horizoncolor = '#c01c28';
+                 fog = 0.9;
+             }
+
+
+             return `preset: checkerboard; active: true; seed: 8; skyType: gradient; skyColor:  ${skycolor}; horizonColor: ${horizoncolor}; fog: ${fog}; lightPosition: [object Object]; ground: noise; groundYScale: 4.18; groundTexture: squares; groundColor: #252525; groundColor2: #111111; dressing: trees; dressingAmount: 20; dressingColor: #888b1d; dressingScale: 1; dressingVariance: [object Object]; gridColor: #c5a543; grid: 1x1; gridColor: #ffffff`;
+         }
      }
  }
 </script>
