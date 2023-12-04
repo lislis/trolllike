7038ba2d5d184c7fabb88065cb09542db22ef141
lislis <mail@lislis.de>
2023-12-04 23:36:53 +0100

    change rest url path

--- inc/register-apps.php
+++ inc/register-apps.php
@@ -92,7 +92,8 @@ function troll_scripts() {
             'wpData',
             array(
                 'template_directory_uri' => get_stylesheet_directory_uri(), // child theme directory path.
-                'rest_url' => untrailingslashit( esc_url_raw( rest_url() ) ),
+                //'rest_url' => untrailingslashit( esc_url_raw( rest_url() ) ),
+                'rest_url' => get_site_url() . '/index.php/wp-json',
                 'base_url' => get_site_url(),
                 'main_filter_cat' => get_option('main_filter_cat'),
                 'youtube_tag_id' => get_option('youtube_tag'),
